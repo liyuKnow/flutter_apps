8e00c47702259d161fa16eff511b2b40457981a4
liyuKnow <kidus2011@gmail.com>
2022-10-11 23:52:33 -0700

    initial setup

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../core/color.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const SizedBox(
+            height: 25,
+          ),
+          const Text(
+            "let's plant with us",
+            style: TextStyle(
+              fontSize: 22.0,
+              letterSpacing: 1.8,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            'Bring nature home',
+            style: TextStyle(
+              color: grey,
+              fontSize: 16,
+              letterSpacing: 1.8,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 450,
+            width: 450,
+            child: Image.asset('assets/images/Asset1.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}
